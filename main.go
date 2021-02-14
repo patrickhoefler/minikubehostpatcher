ecbfc3dd0187e377ba07e01c08275845bbce5dad
Patrick Hoefler <patrick.hoefler@gmail.com>
2021-02-14 15:22:06 +0100

    Increase max startup time to 60 secs

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 
 	fmt.Println("\nChecking DNS resolution of host.minikube.internal ...")
 	var resolvedHostIP string
-	for i := 0; i < 3; i++ {
+	for i := 0; i < 6; i++ {
 		resolvedHostIP, err = kubectl.QueryHostIPFromCoreDNS()
 		if err == nil {
 			break
